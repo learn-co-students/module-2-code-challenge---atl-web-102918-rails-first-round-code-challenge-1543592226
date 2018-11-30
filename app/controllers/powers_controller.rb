88143e6a6eb7f02bd981c4c66be5c55bdc25cef4
Andrew Jones <joneseyit@protonmail.com>
2018-11-30 12:11:57 -0500

    finished

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,16 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  # def search
+  #   @power = Power.find_by name:(params[:id])
+  # end
+  private
+  def power_params
+    params.require(:power).permit(:power, :heroine)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :show]
+  resources :heroines, only: [:index, :show, :create, :new]
+  # get 'powers/search/'  to: 'power#show'
+  # post '/powers' to: 'power#show'
 end
