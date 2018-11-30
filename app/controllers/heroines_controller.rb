0719b6d9d28012962b3a476127cc876d3a6431b4
github username <github email address>
2018-11-30 11:56:42 -0500

    Finished Mod 2 Code Challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    # @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
+  end
+
+  def show
+    @powers = Power.all
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @powers = Power.all
+    @heroine = Heroine.new
+  end
+
+  def create
+    @powers = Power.all
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
+
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,21 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :name, presence: true
+  validates :super_name, presence: true, uniqueness: true
+  validates :power_id, presence: true
+
+
+  def self.search(search)
+    if search
+      power = Power.find_by(name: search)
+      if power
+        self.where(power_id: power)
+      else
+        Heroine.all
+      end
+    else
+      Heroine.all
+    end
+  end
+
 end
