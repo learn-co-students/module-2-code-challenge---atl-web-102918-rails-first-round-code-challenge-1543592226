8ad747b4843ded97bebcafd9c48dc6c3c5848708
Cory <coadams9@gmail.com>
2018-11-30 12:13:40 -0500

    Cory

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def update
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.update
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end 
+
+  private
+
+    def heroine_params
+      params.require(:heroine).permit(:name, :super_name, :power)
+    end
 end
